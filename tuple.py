5a55ea18b0e0b78b8b119e59ae0b72aa31d0c9bd
Python_Basics <prajwalkatkar17@gmail.com>
2026-01-12 00:11:20 +0530

    tuple

---
+++ tuple.py
@@ -0,0 +1,9 @@
+#tuple are immutalbe 
+a=() #empty tuple
+A = (1) #  it is not tupple 
+print(type(A)) #showing datatype as int 
+
+# to make a tuple we need a comma 
+a = (1,) #now this is tuple
+print(type(a)) 
+
