11cd153d8965c8a04756ed37836f31982fab25f4
Python_Basics <prajwalkatkar17@gmail.com>
2026-01-11 22:55:59 +0530

    list indexing

---
+++ list_indexing.py
@@ -0,0 +1,10 @@
+# List and tupples
+
+# we have to store data in the form of array or list if we have different types of datatype
+
+hello = ["Prajwal", "Rishabh", 17 , 54.2,"india","canada"]
+hello[0] = "Raistar"
+print(hello[0])
+
+# we can change the data from the main list because list are mutable
+#but strings are not 
