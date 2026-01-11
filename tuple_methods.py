da2f21f58836abaad5a9f6afe224bbc52c7fa25c
Python_Basics <prajwalkatkar17@gmail.com>
2026-01-12 00:14:52 +0530

    tuple_methods

---
+++ tuple_methods.py
@@ -0,0 +1,8 @@
+a=(2,34,78,"Rohan","Sharma",34)
+no = a.count(34)
+print(no)#tell  the total number of integer or string 
+#in this case output is 2 because 34 is 2 times
+
+
+hell = a.index("Rohan")
+print(hell)
