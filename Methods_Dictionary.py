a362221f737c6c1926caab7e2b9b02d8d9c85d9b
Python_Basics <prajwalkatkar17@gmail.com>
2026-01-13 09:29:32 +0530

    dictionary_methods

---
+++ Methods_Dictionary.py
@@ -0,0 +1,28 @@
+a = {
+    "Prajwal" :54,
+    "Harsh" :98,
+     "Rohan" : 100,
+     5:"Jayesh"
+}
+# print(a.items())
+# print(a.keys())
+# print(a.values())
+# a.update({"Harsh":100})  #update can be done to update as well as to add something to dict
+# print(a)
+
+# a.update({"Harry":88})
+# print(a)
+
+
+
+# print(a.get("Prajwal")) #now here both will give same output
+# print(a["Prajwal"])
+
+#  but if the data doest not exist then 
+# print(a.get("Prajwal2")) #it will give none
+# print(a["Prajwal2"]) #it will give  error 
+
+
+# a.clear()
+# print(a) 
+
