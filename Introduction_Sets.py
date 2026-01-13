bc1e6409d6a814b3c152af90c5571b6b0dac057a
Python_Basics <prajwalkatkar17@gmail.com>
2026-01-13 19:13:40 +0530

    introduction Sets

---
+++ Introduction_Sets.py
@@ -0,0 +1,7 @@
+# to create a empty set 
+a = set()
+b = {} # dont create empty set like this , this will create a empty dictionary 
+s = {1,2,5}
+ # in set number cant be repeated  
+k= {2,5,6,6,6,5,5}
+print(k) #output {2,5,6}
