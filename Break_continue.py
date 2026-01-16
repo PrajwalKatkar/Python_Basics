64e6675ca27e8aa4f8d3a997e7b54f5b95270894
Python_Basics <prajwalkatkar17@gmail.com>
2026-01-17 00:06:38 +0530

    break and continue

---
+++ Break_continue.py
@@ -0,0 +1,12 @@
+for i in range(0,31):
+    if(i==20):
+        break
+    print(i)  
+    # its break the loop where we have put the condition 
+
+
+for i in range(0,31):
+    if(i==20):
+        continue
+    print(i)  
+# its leaves the key that we have used in if condition  
