2a7668f97a88a53d15eb21547188896fb163f6e6
Prajwal Katkar <prajwalkatkar17@gmail.com>
2026-01-20 21:45:40 +0530

    Add files via upload

---
+++ funtions.py
@@ -0,0 +1,37 @@
+# a = int(input("Enter Your Number : "))
+# b = int(input("Enter Your Number : "))
+# c = int(input("Enter Your Number : "))
+
+# avg = (a+b+c)/3
+
+# print(avg)
+# a = int(input("Enter Your Number : "))
+# b = int(input("Enter Your Number : "))
+# c = int(input("Enter Your Number : "))
+
+# avg = (a+b+c)/3
+
+# print(avg)
+# a = int(input("Enter Your Number : "))
+# b = int(input("Enter Your Number : "))
+# c = int(input("Enter Your Number : "))
+
+# avg = (a+b+c)/3
+
+# print(avg)
+# if i want to calculate this 50 times it would be difficult to do so functions are used 
+
+
+# Function defination
+def avg():
+    a = int(input("Enter Your Number : "))
+    b = int(input("Enter Your Number : "))
+    c = int(input("Enter Your Number : "))
+
+    avg = (a+b+c)/3
+    print(avg)
+# funtion call 
+avg()
+avg()
+avg()
+avg()
