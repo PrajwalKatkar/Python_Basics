69b0c4b4507ea88c414d3fe1a6a335da25ff2e32
Prajwal Katkar <prajwalkatkar17@gmail.com>
2026-01-30 02:26:15 +0530

    Add files via upload

---
+++ Introduction_class.py
@@ -0,0 +1,8 @@
+class Employee:
+    name = "Harry" #this are class attributes 
+    language = "Java"
+    salary = 1200000  
+
+harry = Employee()
+harry.age = 20 #this is instance attribute
+print(harry.name ,harry.age, harry.language,harry.salary)    
